713305e6e21cba7229b787fafc2288db30e5cb70
Justin Chang <changguangyu@yahoo.com>
2021-09-14 22:35:38 -0700

    Added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
+import { Navbar } from './components';
 
 function App() {
   return (
-    <>
-    </>
+    <Router>
+      <Navbar />
+
+      <Route path="/history" />
+      <Route path="/brotherhood" />
+      <Route path="/professionalism" />
+      <Route path="/service" />
+
+      <Route path="/actives" />
+      <Route path="/alumni" />
+
+      <Route path="/rush" />
+    </Router>
   );
 }
 
