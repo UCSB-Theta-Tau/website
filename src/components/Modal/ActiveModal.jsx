476dac60aa2e5f61e16e12ae90a91f4eec217aa0
Justin Chang <37165464+just-in-chang@users.noreply.github.com>
2021-09-26 04:16:53 -0700

    Change background color and sizing of active page (#19)

--- src/components/Modal/ActiveModal.jsx
+++ src/components/Modal/ActiveModal.jsx
@@ -3,7 +3,7 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import {
   Modal, ModalInner, ModalCloseContainer, ModalClose, ModalSubtitle, ModalText, LinkedinButton,
-  ModalProfile, ModalProfileImage,
+  ModalProfile, ModalProfileImage, ModalProfileImageContainer,
 } from './Modal.styles';
 
 const ActiveModal = (props) => {
@@ -19,8 +19,6 @@ const ActiveModal = (props) => {
         </ModalCloseContainer>
         <ModalProfile>
           <div style={{ width: '100%' }}>
-            <ModalSubtitle>Name</ModalSubtitle>
-            <ModalText>{props.name}</ModalText>
             <ModalSubtitle>Year/Major</ModalSubtitle>
             <ModalText>{props.year}</ModalText>
             <ModalSubtitle>Pledge Class</ModalSubtitle>
@@ -29,18 +27,21 @@ const ActiveModal = (props) => {
             </ModalText>
             <ModalSubtitle>Hometown</ModalSubtitle>
             <ModalText>{props.hometown}</ModalText>
+            <ModalSubtitle>Why I joined Theta Tau</ModalSubtitle>
+            <ModalText>{props.why}</ModalText>
+            <ModalSubtitle>What I enjoy about Theta Tau</ModalSubtitle>
+            <ModalText>{props.what}</ModalText>
+            <ModalSubtitle>About me</ModalSubtitle>
+            <ModalText>{props.about}</ModalText>
           </div>
-          <ModalProfileImage src={`${process.env.PUBLIC_URL}/static/profiles/${props.pledgeClass}/${props.img}`} alt={props.name} />
+          <ModalProfileImageContainer>
+            <ModalProfileImage src={`${process.env.PUBLIC_URL}/static/profiles/${props.pledgeClass}/${props.img}`} alt={props.name} />
+            <ModalSubtitle>{props.name}</ModalSubtitle>
+            <a target="_blank" href={props.linkedin} rel="noreferrer">
+              <LinkedinButton icon={faLinkedin} />
+            </a>
+          </ModalProfileImageContainer>
         </ModalProfile>
-        <ModalSubtitle>Why I joined Theta Tau</ModalSubtitle>
-        <ModalText>{props.why}</ModalText>
-        <ModalSubtitle>What I enjoy about Theta Tau</ModalSubtitle>
-        <ModalText>{props.what}</ModalText>
-        <ModalSubtitle>About me</ModalSubtitle>
-        <ModalText>{props.about}</ModalText>
-        <a target="_blank" href={props.linkedin} rel="noreferrer">
-          <LinkedinButton icon={faLinkedin} />
-        </a>
       </ModalInner>
     </Modal>
   );
