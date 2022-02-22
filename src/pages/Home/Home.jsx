06806b1144a54cbef3abb9410db9e050c9bea286
SuryaPugal <spugal@ucsb.edu>
2022-02-21 22:22:08 -0800

    brotherhood --> family

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => (
       cards={[{
         img: '/static/home/pillars/service.png',
         title: 'service',
-        text: 'The surrounding communities are very important to us. We believe that giving back to the community not only benefits the communities, but our brotherhood as well. Working alongside with our brothers creates a strong bond and allows us to grow and get involved.',
+        text: 'The surrounding communities are very important to us. We believe that giving back to the community not only benefits the communities, but our members as well! Working alongside with our brothers creates a strong bond and allows us to grow and get involved.',
       },
       {
         img: '/static/home/pillars/professionalism.png',
@@ -21,8 +21,8 @@ const Home = () => (
         text: 'At Theta Tau, we assist all of our members develop the professional skills needed in life after graduation. From resume workshops to company info sessions, our chapter strives to provide the tools needed for any career path. With the variety of skills and mentorship available from our network of engineers, one has the support needed to achieve the high level of professionalism we see in our members.',
       }, {
         img: '/static/home/pillars/brotherhood.png',
-        title: 'brotherhood',
-        text: 'Promoting friendship and collaboration between fraternity members, brotherhood is one of the three key pillars of Theta Tau. Brothers of Theta Tau forge tightly-knit relationships through various brotherhood events, including socials, retreats, and friendly competition.',
+        title: 'Family',
+        text: 'Promoting friendship and collaboration between fraternity members, family is one of the three key pillars of Theta Tau. Actives of Theta Tau forge tightly-knit relationships through various social events, including hangouts, retreats, and friendly competition.',
       }]}
     />
   </>
