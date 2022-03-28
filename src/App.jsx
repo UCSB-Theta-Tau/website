2e5d75691e472198e5dbd7eaa94f8f5ab0bee228
Justin Chang <changguangyu@yahoo.com>
2022-03-27 20:55:45 -0700

    Browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Navbar } from './components';
 import { Actives, Alumni, Brotherhood, History, Home, Professionalism, Rush, Service } from './pages';
 
--- src/pages/Rush/Rush.styles.js
+++ src/pages/Rush/Rush.styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Banner = styled.div`
     min-height: 100vh;
-    background: url(/Website/static/rush/banner.png);
+    background: url(/static/rush/banner.png);
     background-size: cover;
     background-position: center;
 `;
